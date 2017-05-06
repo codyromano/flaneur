772f6422aeb1117a4470339d1b57fbc9339d3110
Cody Romano <codyromano@gmail.com>
2017-05-06 15:27:53 -0700

    Landing Page

--- src/script/pages/LandingPage.jsx
+++ src/script/pages/LandingPage.jsx
@@ -1,14 +1,28 @@
 import React, {PropTypes} from 'react';
 import styles from 'styles/LandingPage.scss';
+import {mainContainer} from 'styles/BasePage.scss';
+import Button from 'components/Button.jsx';
+import {routerRedirect} from 'flaneur-utils';
 
 class LandingPage extends React.Component {
   constructor() {
     super();
+    this.onStartClick = this.onStartClick.bind(this);
+  }
+  onStartClick() {
+    routerRedirect('pick-region');
   }
   render() {
     return (
-      <div className={styles.mainContainer}>
-        Landing Page
+      <div className={styles.background}>
+        <div className={mainContainer}>
+          <h1 className={styles.logo}>Flaneur</h1>
+          <h2 className={styles.subheading}>(Noun) A person who wanders through
+          a city for the sake of exploring it.</h2>
+
+          <Button onClick={this.onStartClick}
+            block={false}>Start Exploring</Button>
+        </div>
       </div>
     );
   }
