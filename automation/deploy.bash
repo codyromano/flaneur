5d9b6f166a52e3030a6aaf88c253d1a3b2cf4e3e
Cody Romano <codyromano@gmail.com>
2017-05-06 16:55:03 -0700

    Automate deploy script

---
+++ automation/deploy.bash
@@ -0,0 +1,13 @@
+DESKTOP=/Users/Cody/Desktop
+DEV_FOLDER=$DESKTOP/flaneur
+PROD_FOLDER=$DESKTOP/flaneur-web
+SHARE_LINK="http://codyromano.com:8081/flaneur-web/"
+
+rm -rf $PROD_FOLDER
+cp -r $DEV_FOLDER $PROD_FOLDER
+rm -rf $PROD_FOLDER/node_modules/
+rm -rf $PROD_FOLDER/.git
+scp -r $PROD_FOLDER root@codyromano.com:/home/public/
+
+echo $SHARE_LINK | pbcopy
+echo "Copied $SHARE_LINK to clipboard"
