61125703c8bde6858d86711aaabe40259ade527e
Cody Romano <codyromano@gmail.com>
2017-05-06 11:14:18 -0700

    Use SASS with CSS modules

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     loaders: [
       {
         test: /\.css$/,
-        loader: 'style!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]' 
+        loader: 'style!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!sass-loader' 
       },
       {
         test: /\.json$/,
