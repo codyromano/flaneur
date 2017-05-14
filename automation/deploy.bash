ff8aea3e9f54c3ebae61b0bf41717ffb33da5bdc
Cody Romano <codyromano@gmail.com>
2017-05-14 16:53:28 -0700

    Optimize deployment script

--- automation/deploy.bash
+++ automation/deploy.bash
@@ -3,6 +3,8 @@ DEV_FOLDER=$DESKTOP/flaneur
 PROD_FOLDER=$DESKTOP/flaneur-web
 SHARE_LINK="http://codyromano.com:8081/flaneur-web/"
 
+echo "Creating prod Webpack bundle. This could take a while."
+webpack --optimize-minimize --define process.env.NODE_ENV="'production'"
 rm -rf $PROD_FOLDER
 cp -r $DEV_FOLDER $PROD_FOLDER
 rm -rf $PROD_FOLDER/node_modules/
