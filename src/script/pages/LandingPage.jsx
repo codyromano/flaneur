7b5d116ce95241a07b0d644ae07253e689ce7239
Cody Romano <codyromano@gmail.com>
2017-05-14 22:12:38 -0700

    Improve Landing Page design

--- src/script/pages/LandingPage.jsx
+++ src/script/pages/LandingPage.jsx
@@ -1,4 +1,4 @@
-import React, {PropTypes} from 'react';
+import React from 'react';
 import styles from 'styles/LandingPage.scss';
 import {mainContainer} from 'styles/BasePage.scss';
 import Button from 'components/Button.jsx';
@@ -17,7 +17,7 @@ class LandingPage extends React.Component {
     return (
       <div className={styles.background}>
         <div className={landingContainer}>
-          <h1 className={styles.logo}>flâ·neur</h1>
+          <h1 className={styles.logo}>flâneur</h1>
           <h2 className={styles.subheading}>(Noun) A person who wanders through
           a city for the sake of exploring it.</h2>
 
