60fff4665710fa08c5f46ba0ea94a9efcad2ad45
Cody Romano <codyromano@gmail.com>
2017-05-20 14:04:34 -0700

    Assign points required

--- src/script/filters/mapActivitiesAssignExplorerPoints.js
+++ src/script/filters/mapActivitiesAssignExplorerPoints.js
@@ -4,6 +4,16 @@ export default function mapActivitiesAssignExplorerPoints(/*activities*/) {
       activity.pointsRewarded = Math.max(1,
         Math.round(activity.difficulty * 1000)
       );
+
+      /* The tentative plan is to tier activities based on difficulty.
+      The lowest 25% are available immediately. To unlock more difficult
+      ones, you need to earn Explorer Points. */
+      if (activity.difficulty <= 0.25) {
+        activity.pointsRequired = 0;
+      } else {
+        activity.pointsRequired = activity.pointsRewarded;
+      }
+
     } else {
       throw new Error(`Activity must have an assigned difficulty
         before explorer point values can be calculated.`);
